3def599378fd05a98322b9ce959d5ec568b1f678
grayboneonline <grayboneonline@gmail.com>
2015-10-26 23:51:47 +0700

    Add classes for products

---
+++ SOLASolutions/DataAccess/SOLA.DataAccess.Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using PetaPoco;
+
+namespace SOLA.DataAccess.Models
+{
+    [TableName("Products")]
+    [PrimaryKey("ProductId")]
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SOLASolutions/Presentation/SOLA.WebApi.Models/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SOLA.WebApi.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+    }
+}
