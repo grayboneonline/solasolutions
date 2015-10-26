2ec3e4b54d7d52e36dc771036289aa18dd291b84
grayboneonline <grayboneonline@gmail.com>
2015-10-26 18:52:56 +0700

    Add IUserSession

---
+++ SOLASolutions/Common/SOLA.Cache/Contracts/IUserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace SOLA.Cache.Contracts
+{
+    public interface IUserSession
+    {
+        int UserId { get; set; }
+        string UserName { get; set; }
+        IRefreshToken RefreshToken { get;set; }
+    }
+}
